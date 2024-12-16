9653afc1c2a851a89c2b2277bf75cfd5d2b1fe0f
Apple <apple@Apples-MacBook-Pro.local>
2024-12-16 05:02:29 +0400

    tg motion setup scripts

---
+++ install_camera.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+# Проверка обязательных переменных
+REQUIRED_VARS=("TELEGRAM_API_TOKEN" "TELEGRAM_BOT_ID" "TELEGRAM_CHANNEL_ID")
+OPTIONAL_VAR="CAMERA_PORT"
+
+# Проверка, что скрипт запущен от имени root
+if [[ "$EUID" -ne 0 ]]; then
+  echo "Скрипт должен быть запущен с правами root. Запустите его с помощью sudo."
+  exit 1
+fi
+
+# Функция для вывода справки
+print_help() {
+  echo "Использование скрипта:"
+  echo "  Перед запуском необходимо задать следующие переменные окружения:"
+  echo "  Обязательные:"
+  echo "    TELEGRAM_API_TOKEN   - API токен Telegram бота."
+  echo "    TELEGRAM_BOT_ID      - ID Telegram бота."
+  echo "    TELEGRAM_CHANNEL_ID  - ID Telegram канала."
+  echo ""
+  echo "  Необязательные:"
+  echo "    CAMERA_PORT          - Порт для потокового видеосервера первой камеры, последующие получат инкремент на 1"
+  echo ""
+  echo "Пример запуска:"
+  echo "  TELEGRAM_API_TOKEN=<токен> TELEGRAM_BOT_ID=<бот id> TELEGRAM_CHANNEL_ID=<канал id> CAMERA_PORT=8080 ./script.sh"
+}
+
+# Проверяем наличие всех обязательных переменных
+for var in "${REQUIRED_VARS[@]}"; do
+  if [ -z ${var+x} ]; then
+    echo "Ошибка: Переменная $var не задана."
+    print_help
+    exit 1
+  fi
+done
+
+# Проверяем необязательную переменную и выводим сообщение, если она не задана
+if [ -z ${OPTIONAL_VAR+x} ]; then
+  echo "Предупреждение: Переменная CAMERA_PORT не задана. Будет использовано значение 8080."
+fi
+
+
+# Создаём пользователя tg_motion, если он ещё не существует
+if ! id "tg_motion" &>/dev/null; then
+  echo "Создаём пользователя tg_motion..."
+  useradd -m -s /bin/bash tg_motion
+  if [ $? -eq 0 ]; then
+    echo "Пользователь tg_motion успешно создан."
+  else
+    echo "Не удалось создать пользователя tg_motion."
+    exit 1
+  fi
+else
+  echo "Пользователь tg_motion уже существует."
+fi
+
+# Переходим в домашнюю директорию пользователя tg_motion
+home_dir=$(eval echo ~tg_motion)
+if [ -d "$home_dir" ]; then
+  echo "Найдена домашнюю директория пользователя tg_motion: $home_dir"
+else
+  echo "Домашняя директория пользователя tg_motion не найдена."
+  exit 1
+fi
+
+sudo apt-get install -y python3-pip
+sudo apt-get install -y python3-venv
+sudo apt-get install -y v4l-utils
+
+sudo mkdir /var/log/supervisor
+sudo mkdir /var/log/motion
+sudo touch /var/log/motion/motion.log
+sudo cmod 777 /var/log/motion/motion.log
+
+mkdir .ssh
+mkdir .motion
+
+sudo apt-get install -y supervisor
+sudo apt-get install -y motion
+
+sudo cp supervisor/motion.conf /etc/supervisor/conf.d/motion.conf
+echo environment=HOME=\"$home_dir\",TELEGRAM_API_TOKEN=\"$TELEGRAM_API_TOKEN\",TELEGRAM_BOT_ID=\"$TELEGRAM_BOT_ID\",TELEGRAM_CHANNEL_ID=\"$TELEGRAM_CHANNEL_ID\" >> /etc/supervisor/conf.d/motion.conf
+
+sudo cp motion/motion.conf /etc/motion/motion.conf
+sudo cp tg_message.py $home_dir/tg_message.py
+
+cd $home_dir
+python3 -m venv venv_tg
+venv_tg/bin/pip3 install python-telegram-bot==13.15
+
+sudo supervisorctl reread
+sudo supervisorctl update
+sudo supervisorctl start
+
+
+v4l2-ctl --list-devices
---
+++ tg_message.py
@@ -0,0 +1,47 @@
+from telegram import Bot
+import sys
+import os
+
+bot_token = os.getenv("TELEGRAM_API_TOKEN", "")
+bot_user_name = os.getenv("TELEGRAM_BOT_ID", "")
+chat_id = os.getenv("TELEGRAM_CHANNEL_ID", "")
+
+lan_ip = os.popen("hostname -I").read().strip()
+wan_ip = os.popen("curl -s ipinfo.io/ip").read().strip()
+
+global TOKEN
+TOKEN = bot_token
+bot = Bot(token=TOKEN)
+
+
+camera_id = sys.argv[1]
+cam = camera_id.split(':')[0]
+port = camera_id.split(':')[1]
+camera = f'{cam}\n\nhttp://{lan_ip}:{port}\n\nhttp://{wan_ip}:{port}'
+event_type = sys.argv[2]
+
+if event_type == 'event':
+    bot.send_message(chat_id=chat_id,text = f"Обнаружено движение: камера {camera}")
+if event_type == 'lost':
+    bot.send_message(chat_id=chat_id,text = f"Камера {camera} отключена")
+if event_type == 'found':
+    bot.send_message(chat_id=chat_id,text = f"Камера {camera} подключена")
+if event_type == 'photo':
+    path = sys.argv[3]
+    try:
+        bot.send_photo(chat_id=chat_id,photo=open(path,'rb'), caption=camera,timeout=1000000)
+        os.remove(path)
+    except:
+        os.remove(path)
+        bot.send_message(chat_id=chat_id,text = f"Не получилось отправить фото {path} с {camera}")
+if event_type == 'video':
+    path = sys.argv[3]
+    try:
+        if os.stat(path).st_size > 0:
+            bot.send_video(chat_id=chat_id,video=open(path,'rb'), supports_streaming=True,caption=camera,timeout=1000000)
+        else:
+            bot.send_message(chat_id=chat_id,text = f"Пустой файл {path} с {camera}")
+        os.remove(path)
+    except:
+        os.remove(path)
+        bot.send_message(chat_id=chat_id,text = f"Не получилось отправить видео {path} с {camera}")
